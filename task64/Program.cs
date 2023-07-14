0c40e5fe3d69ad26d2d97705c49376873950f6d0
Kirill Grinkov <79636456163@yandex.ru>
2023-07-14 09:17:55 +0400

    addtask64

---
+++ task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+Console.Write("Задайте значение N: ");
+ int N = int.Parse(Console.ReadLine() ?? "");
+
+NumberInOrder(N);
+
+
+int NumberInOrder(int N, int M = 0)
+{
+    if (N == M) 
+        return N;
+    else Console.Write($"{NumberInOrder(N, M + 1)}, ");
+        return M;
+ }
+
